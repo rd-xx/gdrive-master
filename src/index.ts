b26e068c763d4ab043a3aa361f39c324586efd94
rdx <31172722+rd-xx@users.noreply.github.com>
2022-09-20 14:41:05 +0200

    create translations

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('hello world');
+import { I18n } from '../node_modules/i18n-js/typings/I18n';
+import translationFr from '../locales/fr.json';
+import translationEn from '../locales/en.json';
+import translationPt from '../locales/pt.json';
+
+const i18n = new I18n();
+i18n.store(translationEn);
+i18n.store(translationFr);
+i18n.store(translationPt);
+
+// const locale = Intl.DateTimeFormat().resolvedOptions().locale;
+// console.log(locale);
+
+async function main() {}
