41537301e7e96f1849e66aabb08874bb275ec43b
rdx <31172722+rd-xx@users.noreply.github.com>
2022-09-30 23:12:33 +0200

    basic config manager

---
+++ src/managers/config.manager.ts
@@ -0,0 +1,31 @@
+import { readJson, writeJson } from 'fs-extra';
+
+export default class ConfigManager {
+	// Absolute path to the config file
+	location: string;
+	firstTime: boolean | undefined;
+	complementsToken: string | undefined;
+
+	constructor(configPath: string) {
+		this.location = configPath;
+	}
+
+	// Load the config file and set the properties
+	async load() {
+		const configFile = await readJson(this.location, { throws: false });
+		if (!configFile) {
+			this.firstTime = true;
+			return;
+		}
+
+		if (configFile.complementsToken)
+			this.complementsToken = configFile.complementsToken;
+	}
+
+	// Create the config file
+	async create() {
+		await writeJson(this.location, {
+			complementsToken: this.complementsToken || ''
+		});
+	}
+}
