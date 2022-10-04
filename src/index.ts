aa412dfbfea9dbc6b621e678546bceb4b8a8fc8d
rdx <reydux95@gmail.com>
2022-10-04 22:14:27 +0200

    setup i18n

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
 import { join, normalize } from 'path';
 import { I18n } from 'i18n';
 
+// Setup i18n
 const i18n = new I18n({
-	locales: ['en', 'fr', 'pt'],
-	directory: normalize(join(__dirname, '..', 'locales'))
-});
+    locales: ['en', 'fr', 'pt'],
+    directory: normalize(join(__dirname, '..', 'locales'))
+  }),
+  locale = Intl.DateTimeFormat().resolvedOptions().locale;
+
+if (locale.includes('pt')) i18n.setLocale('fr');
+else if (locale.includes('pt')) i18n.setLocale('pt');
+else i18n.setLocale('en');
+
 
 async function main() {}
