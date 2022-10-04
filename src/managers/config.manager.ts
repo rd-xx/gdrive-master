ca2a26d68a888f4d249b639cde592d8bea490d77
rdx <reydux95@gmail.com>
2022-10-04 22:14:05 +0200

    update all files to follow the new linting rules

--- src/managers/config.manager.ts
+++ src/managers/config.manager.ts
@@ -1,31 +1,31 @@
 import { readJson, writeJson } from 'fs-extra';
 
 export default class ConfigManager {
-	// Absolute path to the config file
-	location: string;
-	firstTime: boolean | undefined;
-	complementsToken: string | undefined;
+  // Absolute path to the config file
+  location: string;
+  firstTime: boolean | undefined;
+  complementsToken: string | undefined;
 
-	constructor(configPath: string) {
-		this.location = configPath;
-	}
+  constructor(configPath: string) {
+    this.location = configPath;
+  }
 
-	// Load the config file and set the properties
-	async load() {
-		const configFile = await readJson(this.location, { throws: false });
-		if (!configFile) {
-			this.firstTime = true;
-			return;
-		}
+  // Load the config file and set the properties
+  async load() {
+    const configFile = await readJson(this.location, { throws: false });
+    if (!configFile) {
+      this.firstTime = true;
+      return;
+    }
 
-		if (configFile.complementsToken)
-			this.complementsToken = configFile.complementsToken;
-	}
+    if (configFile.complementsToken)
+      this.complementsToken = configFile.complementsToken;
+  }
 
-	// Create the config file
-	async create() {
-		await writeJson(this.location, {
-			complementsToken: this.complementsToken || ''
-		});
-	}
+  // Create the config file
+  async create() {
+    await writeJson(this.location, {
+      complementsToken: this.complementsToken || ''
+    });
+  }
 }
