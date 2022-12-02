af3697ea5b3232c88d1a728e3a3a43d26ca44d1f
rdx <reydux95@gmail.com>
2022-12-03 00:25:15 +0100

    transfer piece of code to the standalone mode

--- src/modes/standalone.mode.ts
+++ src/modes/standalone.mode.ts
@@ -1,1 +1,14 @@
-export default async function standaloneMode() {}
+export default async function standaloneMode() {
+  // const shouldSave = await askSaveKeys();
+  // if (shouldSave === undefined) return;
+  // // Save the keys
+  // if (shouldSave) {
+  //   const email = getEmail(),
+  //     fileName = await saveKeys(email, keys);
+  //   if (!fileName) return;
+  //   console.log(t('prompts.keys.save.saved'), chalk.cyan(fileName));
+  // } else {
+  //   console.log('');
+  //   for (const key of keys) console.log(key);
+  // }
+}
